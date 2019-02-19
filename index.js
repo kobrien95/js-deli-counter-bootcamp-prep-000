37f0540b7f23852244e7d4b5194a8fe1e972cca9
Kristian O'Brien <kristianobrien95@gmail.com>
2019-02-19 18:12:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function currentLine(katzDeli) {
     i++;
   }
   if (katzDeli.length === 0) {
-    return "The line is currently empty.";
+    return "The line is currently 1.${name}.";
   } else
-  return(`Currently serving, line shift.`)
+  return(`Currently s`)
 }
